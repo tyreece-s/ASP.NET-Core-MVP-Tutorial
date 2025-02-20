d19b51db2832acfa178f65320db78df61a6d2c7e
tyreece-s <st10439074@vcconnect.edu.za>
2025-02-21 00:01:04 +0200

    Did "Seed the database"

--- MvcMovie/obj/Debug/net9.0/MvcMovie.AssemblyInfo.cs
+++ MvcMovie/obj/Debug/net9.0/MvcMovie.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a9543cf0bbe977095d97556ed90c8cfd5a521865")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0165e8781f9a92a0cb3b1bea3edb3e8869e5ccbf")]
 [assembly: System.Reflection.AssemblyProductAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
