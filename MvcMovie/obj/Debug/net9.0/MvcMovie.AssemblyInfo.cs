517f84adc44eb4413f15e43f2d37171921b9fe4e
tyreece-s <st10439074@vcconnect.edu.za>
2025-02-20 02:08:20 +0200

    Moved gitignore to main branch

--- MvcMovie/obj/Debug/net9.0/MvcMovie.AssemblyInfo.cs
+++ MvcMovie/obj/Debug/net9.0/MvcMovie.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7a4c12d2339dfd856353b61e8d97c4c1b3d574bd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9d8a196d9b85096b187893531960c174817201fd")]
 [assembly: System.Reflection.AssemblyProductAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
