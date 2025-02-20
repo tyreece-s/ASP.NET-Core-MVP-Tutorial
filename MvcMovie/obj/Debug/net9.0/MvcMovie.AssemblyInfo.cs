fc91a1375bb98d98acafc3ab149a0dc28c1406f9
tyreece-s <st10439074@vcconnect.edu.za>
2025-02-20 02:30:20 +0200

    Changed the title, footer, and menu link in the layout file

--- MvcMovie/obj/Debug/net9.0/MvcMovie.AssemblyInfo.cs
+++ MvcMovie/obj/Debug/net9.0/MvcMovie.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e9029981cb76323378151bc9f08dd314bbadce6e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5885af76a3a4e4683ddf55c7ffa7902192a987e8")]
 [assembly: System.Reflection.AssemblyProductAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
