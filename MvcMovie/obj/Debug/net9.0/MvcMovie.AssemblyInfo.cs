a9543cf0bbe977095d97556ed90c8cfd5a521865
tyreece-s <st10439074@vcconnect.edu.za>
2025-02-20 23:40:13 +0200

    Created a database using EF Core Migrations Feature

--- MvcMovie/obj/Debug/net9.0/MvcMovie.AssemblyInfo.cs
+++ MvcMovie/obj/Debug/net9.0/MvcMovie.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a3652722c9d775115b267442aa702526b648b26c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+96d5ebdadab9b86fc2a8b15b862061b0d3c10bd0")]
 [assembly: System.Reflection.AssemblyProductAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
