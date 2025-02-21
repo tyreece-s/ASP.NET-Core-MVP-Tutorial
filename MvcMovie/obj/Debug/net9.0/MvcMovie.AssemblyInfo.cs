3645da650c2f3520ad5fadd3d1b70d1bf4716fa1
tyreece-s <st10439074@vcconnect.edu.za>
2025-02-21 12:27:48 +0200

    added space inbetween "ReleaseDate"

--- MvcMovie/obj/Debug/net9.0/MvcMovie.AssemblyInfo.cs
+++ MvcMovie/obj/Debug/net9.0/MvcMovie.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e27ce939b1aa9ad7db6e9872d19963b42565c308")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b439cbdd2bb50ac1aa33bb1e77cf9edfed2d7854")]
 [assembly: System.Reflection.AssemblyProductAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
