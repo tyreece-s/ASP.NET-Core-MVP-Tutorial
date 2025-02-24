27e2e1d1dd6b801c2ca2dc1e293c5f87c833b863
tyreece-s <st10439074@vcconnect.edu.za>
2025-02-24 23:42:29 +0200

    Added a Search by Genre Option

--- MvcMovie/obj/Debug/net9.0/MvcMovie.AssemblyInfo.cs
+++ MvcMovie/obj/Debug/net9.0/MvcMovie.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d303c987c2f74de37f217271d06e723a8a60d932")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f0d40b3e60be163a1ffceecb4ea7f343c2b8f05f")]
 [assembly: System.Reflection.AssemblyProductAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
